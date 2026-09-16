f2e6ab8a6ab482ed85717614535212b45b263cb0
laptopmannat2346 <tharejamannat1@gmail.com>
2026-09-16 21:12:27 +0530

    Fix CORS for Vercel frontend

--- backend/main.py
+++ backend/main.py
@@ -28,7 +28,10 @@ df["Date"] = pd.to_datetime(df["Date"])
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://frontend-1js1njep9-mannat2.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
